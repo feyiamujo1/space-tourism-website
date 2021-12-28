b933261de0f05e6cb68a70399c43f7744bc74209
feyiamujo1 <feyisayoamujoyegbe@gmail.com>
2021-12-28 17:19:48 +0100

    Fixed mobilr screen display

---
+++ scripts/homepage.js
@@ -0,0 +1,13 @@
+/*For the side bar show and cancel button */
+
+hidesidebar = () =>{
+    var sidebar = document.getElementById("sidebar");
+    sidebar.style.display="none";
+   // document.body.style.position="";
+}
+  
+showsidebar = () =>{
+    var sidebar = document.getElementById("sidebar");
+    sidebar.style.display = "block";
+   // document.body.style.position="fixed";
+}
