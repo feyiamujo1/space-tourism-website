2662b091d1e7a4e70eb9a806441cae5ff5bb46b5
feyiamujo1 <feyisayoamujoyegbe@gmail.com>
2022-01-31 16:19:00 +0100

    Added slideshow feature to the crews page

--- scripts/crew.js
+++ scripts/crew.js
@@ -1,4 +1,5 @@
 var numb = 0;
+var slideIndex = 0;
 
 fetch("./scripts/data.json")
 .then(
@@ -6,7 +7,7 @@ fetch("./scripts/data.json")
         return resp.json();
     })
     .then(function(data){
-        //data.destinations[0];
+        data.destinations[0];
         document.getElementById("role").innerHTML = data.crew[numb].role;
         document.getElementById("name").innerHTML = data.crew[numb].name;
         document.getElementById("crewImage").src = data.crew[numb].images.png;
@@ -15,7 +16,7 @@ fetch("./scripts/data.json")
 
 changeInfo = numb =>{
     fetch("./scripts/data.json")
-.then(
+    .then(
     function(resp){
         return resp.json();
     })
@@ -34,4 +35,24 @@ changeInfo = numb =>{
           dots[i].setAttribute("class", "dot");
         }
     }
+    slideIndex = numb;
 }
+
+showSlides = () => {
+    var i=0;
+    var dots = document.getElementsByClassName("dot");
+    if (slideIndex >= dots.length) {
+        slideIndex = 0
+    }    
+    for (i; i<dots.length; i++){
+        if (i==slideIndex){
+          dots[slideIndex].setAttribute("class", "dot active");
+        }else{
+          dots[i].setAttribute("class", "dot");
+        }
+    }
+    changeInfo(slideIndex);
+    slideIndex++;
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+showSlides();
--- scripts/homepage.js
+++ scripts/homepage.js
@@ -2,11 +2,11 @@
 hidesidebar = () =>{
     var sidebar = document.getElementById("sidebar");
     sidebar.style.display="none";
-   // document.body.style.position="";
+    document.body.style.position="";
 }
   
 showsidebar = () =>{
     var sidebar = document.getElementById("sidebar");
     sidebar.style.display = "block";
-   // document.body.style.position="fixed";
+    document.body.style.position="fixed";
 }
